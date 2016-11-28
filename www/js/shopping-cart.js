eca7c816ce824c5b680904d9b6f4611a318addd3
Lennervald <martin.lennervald@gmail.com>
2016-11-28 14:02:34 +0100

    Added new code for shopping-cart

--- www/js/shopping-cart.js
+++ www/js/shopping-cart.js
@@ -1,116 +1,88 @@
-// Wait for the DOM to load
-$(function(){
+// HEJ! Byt ut all kod i gamla shoppingcart mot detta: 
 
-  // Bind events
+$(function() {
 
-  $('body').on('click','.buy-btn',function(){
-    updateCart($(this), 1);
-  });
+    $('body').on('click', '.buy-btn', function() {
+        updateCart($(this), 1);
+    });
 
-  $('#shoppingCart').on('change','input', function(){
-    updateCart($(this).parents('tr'), 0);
-  });
+    $('#shoppingCart').on('change', 'input', function() {
+        updateCart($(this).parents('tr'), 0);
+    });
 
-  $('#shoppingCart').on('click','.remove', function(){
-    $(this).parents('tr').find('input').val(0);
-    updateCart($(this).parents('tr'), 0);
-  });
+    $('#shoppingCart').on('click', '.remove', function() {
+        $(this).parents('tr').find('input').val(0);
+        updateCart($(this).parents('tr'), 0);
+    });
 
 });
 
-// Update the cart
-function updateCart(me,quantity){
+// Updating the cart
 
-    // The product
+function updateCart(me, quantity) {
+
+    var image = me.attr('data-img');
     var product = me.attr('data-product');
-
-    // The price (divide by one to convert to a number)
     var price = me.attr('data-price') / 1;
+    var productCheck = $('#shoppingCart tr[data-product="' + product + '"]');
+    quantity += (productCheck.find('.quantity input').val() || 0) / 1;
 
-    // Product in cart (if already existing)
-    var productEl = $('#shoppingCart tr[data-product="' + product + '"]');
-
-    // If the product already is in the cart then add the previous
-    // quantity
-    quantity += (productEl.find('.quantity input').val() || 0)/1;
-
-
-    // Calculate row sum
     var rowSum = price * quantity;
 
-    // Add the product if the quantity is larger than 0
-    if(quantity > 0){
-      if(productEl.length){
-        // only update the product if it is the cart already
-        productEl.find('.quantity input').val(quantity);
-        productEl.find('.row-sum').text(decimals(rowSum));
-      }
-      else {
-        // otherwise add the product to the cart
-        $('#shoppingCart table').append( 
-          '<tr class="product" data-product="' + product +
-          '" data-price = "' + price + '">' +
-          '<td>'  + product + '</td>' +
-          '<td class="td-right quantity">' +
-          '<input class="form-control td-right input-sm" type="number" ' +
-          'min ="0" max="999" value="' + quantity + '"></td>' +
-          '<td class="td-right">' + decimals(price) + ' SEK</td>' +
-          '<td class="td-right row-sum">' + decimals(rowSum) + ' SEK</td>' +
-          '<td class="remove"><span class="glyphicon glyphicon-remove" ' +
-          'aria-hidden="true"></span> </td>' +
-          '</tr>'
-        );
-      }
-    }
-    else {
-      // Otherwise remove it
-      productEl.remove();
+    if (quantity > 0) {
+        if (productCheck.length) {
+            productCheck.find('.quantity input').val(quantity);
+            productCheck.find('.row-sum').text(decimals(rowSum) + "SEK");
+        } else {
+            $('#shoppingCart table').append(
+                '<tr class="product" data-product="' + product +
+                '" data-price = "' + price + '">' +
+                '<td class="prod-image">' +
+                '<img src="' + image + '" width="40px" height="28px"></td>' + 
+                '<td>' + product + '</td>' +
+                '<td class="quantity">' +
+                '<input class="form-control td-right input-sm" type="number" ' +
+                'min ="1" max="999" value="' + quantity + '"></td>' +
+                '<td class="item-price">' + decimals(price) + ' SEK</td>' +
+                '<td class="row-sum">' + decimals(rowSum) + ' SEK</td>' +
+                '<td class="remove"><span class="glyphicon glyphicon-remove"></span> </td>' +
+                '</tr>'
+            );
+        }
+    } else {
+        productCheck.remove();
     }
 
-    // Calculate the total sum
     var totalSum = 0;
-    $('#shoppingCart .row-sum').each(function(){
-      var rowSum = $(this).text().replace("SEK", "") / 1;
-      totalSum += rowSum;
+    $('#shoppingCart .row-sum').each(function() {
+        var rowSum = $(this).text().replace("SEK", "") / 1;
+        totalSum += rowSum;
     });
 
-    // Output the total sum
     $('#shoppingCart .total').last().text(decimals(totalSum) + ' SEK');
 
-    // Move the SUM row last
-    $('#shoppingCart table').append( $('#shoppingCart .cart-sum') );
+    $('#shoppingCart table').append($('#shoppingCart .total-cart'));
 
-    // Change the looks of the cart depending on if is empty or not
-    if($('#shoppingCart .product').length < 1){
-      $('#shoppingCart table').hide();
-      $('#shoppingCart .empty-mess').show();
-      $('#shoppingCart .checkout').prop('disabled',true);
+    if ($('#shoppingCart .product').length < 1) {
+        $('#shoppingCart table').hide();
+        $('#shoppingCart .empty-mess').show();
+        $('#shoppingCart .checkout').prop('disabled', true);
 
-    }
-    else {
-      $('#shoppingCart table').show();
-      $('#shoppingCart .empty-mess').hide();
-      $('#shoppingCart .checkout').prop('disabled',false);
+    } else {
+        $('#shoppingCart table').show();
+        $('#shoppingCart .empty-mess').hide();
+        $('#shoppingCart .checkout').prop('disabled', false);
     }
 }
 
-// Make sure all numbers have two decimals
-function decimals(number){
- 
-  // The price as a string (add empty string to number)
-  // (also round to max 2 decimals - to avoid JS rounding error)
-  var str = Math.round(number*100)/100 + "";
- 
-  // Add a decimal sign if missing
-  if(str.indexOf(".") < 0 ){ str += '.'; }
- 
-  // How many decimals do we have?
-  var numberOfDecimals = str.split('.')[1].length;
- 
-  // Add decimals if needed - up to two decimals
-  for(var i = 0; i < 2 - numberOfDecimals; i++){
-    str += "0";
-  }
+// Fixing Decimals
 
-  return str;
+function decimals(number) {
+    var str = Math.round(number * 100) / 100 + "";
+    if (str.indexOf(".") < 0) { str += '.'; }
+    var numberOfDecimals = str.split('.')[1].length;
+    for (var i = 0; i < 2 - numberOfDecimals; i++) {
+        str += "0";
+    }
+    return str;
 }
