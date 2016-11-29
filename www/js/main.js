d6dfdde57ee349488b8ef65a76e360c804b9595c
pellehardstedt <pelle.hardstedt@gmail.com>
2016-11-29 11:02:10 +0100

    url-magic

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,39 @@
+// listen to pop state when the user navigates (forward/backward)
+window.addEventListener("popstate",doStuffOnUrlChange);
+
+// do different stuff depending on url
+var startContent = "";
+function doStuffOnUrlChange(){
+
+    // Store the initial content
+    if(!startContent){
+        startContent = $('.content').html();
+    }
+
+    // Read the urlPath
+    var urlPath = location.pathname.substring(1);
+    
+    // React on different paths
+
+    if(urlPath === "" || urlPath === "index.html"){
+        $('.content').html(startContent);
+    }
+
+    if(urlPath == "product-details"){
+        $.get('product.html', function(data){
+
+            $('.content').html(data);
+        });
+    }
+
+}
+
+// Initial doStuff
+doStuffOnUrlChange();
+
+
 // change content in div.content 
-$('.thumbnail').on('click', '.product-link', function(){
-    $.get('product.html', function(data){
-        $('.content').html(data);
-    });
+$('body').on('click', '.product-link', function(){
+    history.pushState(null,null,"product-details");
+    doStuffOnUrlChange();
 });
