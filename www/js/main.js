b6caa3a67eba95cf61f976b4c9b0cd2c9282ea0d
pellehardstedt <pelle.hardstedt@gmail.com>
2016-11-29 16:14:02 +0100

    support for multiple product pages

--- www/js/main.js
+++ www/js/main.js
@@ -19,12 +19,37 @@ function doStuffOnUrlChange(){
         $('.content').html(startContent);
     }
 
-    if(urlPath == "product-details"){
-        $.get('product.html', function(data){
+    if(urlPath == "product-1"){
+        $.get('product-1.html', function(data){
 
             $('.content').html(data);
         });
     }
+    if(urlPath == "product-2"){
+        $.get('product-2.html', function(data){
+
+            $('.content').html(data);
+        });
+    }
+    if(urlPath == "product-3"){
+        $.get('product-3.html', function(data){
+
+            $('.content').html(data);
+        });
+    }
+    if(urlPath == "product-4"){
+        $.get('product-4.html', function(data){
+
+            $('.content').html(data);
+        });
+    }
+
+    // if(urlPath == "product-details"){
+    //     $.get('product.html', function(data){
+
+    //         $('.content').html(data);
+    //     });
+    // }
 
 }
 
@@ -33,9 +58,23 @@ doStuffOnUrlChange();
 
 
 // change content in div.content 
-$('body').on('click', '.product-link', function(){
-    history.pushState(null,null,"product-details");
+$('body').on('click', '.product-link-1', function(){
+    history.pushState(null,null,"product-1");
     doStuffOnUrlChange();
 });
 
+$('body').on('click', '.product-link-2', function(){
+    history.pushState(null,null,"product-2");
+    doStuffOnUrlChange();
+});
+
+$('body').on('click', '.product-link-3', function(){
+    history.pushState(null,null,"product-3");
+    doStuffOnUrlChange();
+});
+
+$('body').on('click', '.product-link-4', function(){
+    history.pushState(null,null,"product-4");
+    doStuffOnUrlChange();
+});
 
